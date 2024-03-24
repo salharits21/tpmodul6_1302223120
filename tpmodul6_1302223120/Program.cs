a7298301a9655015975a5c0b808c9c49b7a24a5b
Faishal Harits Al Fajri <faishalharits@student.telkomuniversity.ac.id>
2024-03-25 05:45:35 +0700

    implementasi design by contract

--- tpmodul6_1302223120/Program.cs
+++ tpmodul6_1302223120/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302223120;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = null;
+
+        // Design Contract
+        try
+        {
+            // Prekondisi
+            video = new SayaTubeVideo("Tutorial Design By Contract - Faishal Harits Al Fajri");
+            for (int i = 0; i < 10000000; i++)
+            {
+                video.IncreasePlayCount(1);
+            }
+        }
+        // tes Exception
+        catch (Exception tes)
+        {
+            Console.WriteLine("Error: " + tes.Message);
+        }
+        finally
+        {
+            // printoutput jika tidak terdeteksi error
+            if (video != null)
+            {
+                video.PrintVideoDetails();
+            }
+        }
+    }
+}
